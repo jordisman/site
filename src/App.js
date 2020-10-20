ca8874b8594cacc040dcdb19b4913e7e6132422c
Jordis Man <jordisman@gmail.com>
2020-10-19 17:49:51 -0700

    Initial clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import css from './style.css';
 
 function App() {
   return (
-    <div>
-      Hello
+    <div className={css.test}>
+    main container
+      <div>Header</div>
+      <div>Intro</div>
+      <div>pro1</div>
+      <div>pro2</div>
+      <div>pro3</div>
+      <div>pro4</div>
+      <div>pro5</div>
+      <div>pro6</div>
+      <div>contact</div>
+      <div>footer</div>
     </div>
   );
 }
