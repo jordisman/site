06307a88cf660ffe03b9d2135c2c1e4d8a61aded
Jordis Man <jordisman@gmail.com>
2020-10-20 20:35:10 -0700

    Added bg image on header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import css from './css/style.css';
+import mt from './images/mt-swit.jpg';
+import one from './images/1.jpg';
 
-const App = () => (
+const App = () => {
+  const intro = "I'm a full-stack software engineer experienced with Javascript, HTML5, CSS3, React, Node.js, Express, MongoDB, MySQL, PostgreSQL, etc. and I always love to learn new technologies. I enjoy creating and collaborating with others making full stack web applications.";
+
+  return (
     <div className="container">
-      <div className="header">
-        <h1>Hi, my name is Jordis Man</h1>
+      <div className="headerContainer">
+
+        <div className="header">
+
+          <h1 className="underline">Hi, my name is Jordis Man</h1>
+          <p className="content">{intro}</p>
+        </div>
       </div>
-      <div className="intro">Intro</div>
+
+
       <div className="item1">pro1</div>
       <div className="item2">pro2</div>
       <div className="item3">pro3</div>
@@ -20,6 +31,7 @@ const App = () => (
       <div className="footer">footer</div>
       <div className="contact">contact</div>
     </div>
-);
+  );
+}
 
 export default App;
